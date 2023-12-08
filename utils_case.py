1f8b2895d2cf63fa71a13c816af5f46bf72bf781
Denise Case <denisecase@users.noreply.github.com>
2023-12-08 15:48:55 -0600

    Update utils_case.py; use main()

--- utils_case.py
+++ utils_case.py
@@ -3,15 +3,16 @@
 import math
 import statistics
 
-mname: str = "Denise"
+my_name: str = "Denise Case"
 course_count: int = 6
 is_experienced: bool = True
 hours_invested: float = 2.5
 numbers: list = [7, 6, 5, 8, 8, 4, 5, 7, 8, 6]
 
-course_count_string: str = f"{name=}"
-is_experienced_string: str = f"{is_experienced}"
-hours_invested_string: str = f"{hours_invested}"
+my_name_string = f"Author: {my_name}"
+course_count_string: str = f"{course_count=}"
+is_experienced_string: str = f"{is_experienced=}"
+hours_invested_string: str = f"{hours_invested=}"
 numbers_string: str = f"{numbers=}"
 
 radius: float = 6.0
@@ -20,12 +21,12 @@ radius_area_string: str = f"Math: Given {radius=}, {area=}"
 
 # stats
 count: int = len(numbers)
-smallest: int = min(numbers)
-largest: int = max(numbers)
+smallest: int = min(numbers)  # not min  - that's a built in function
+largest: int = max(numbers)  # not max  - that's a built in function
 mode: float = statistics.mode(numbers)
 median: float = statistics.median(numbers)
 mean: float = statistics.mean(numbers)
-stdev: float = statistics.stdev(numbers)
+stdev: float = round(statistics.stdev(numbers),2)
 
 my_stats_string: str = f"""
 Recent Daily Water Counts
@@ -39,10 +40,15 @@ Recent Daily Water Counts
 {stdev=}
 """
 
-# display the information
-print(course_count_string)
-print(is_experienced_string)
-print(hours_invested_string)
-print(numbers_string)
-print(radius_area_string)
-print(my_stats_string)
+def main():
+    ''' Display all the output'''
+    print(my_name_string)
+    print(course_count_string)
+    print(is_experienced_string)
+    print(hours_invested_string)
+    print(numbers_string)
+    print(radius_area_string)
+    print(my_stats_string)
+
+# call main and display all the output
+main()
