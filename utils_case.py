9cce64dbc98180cf563e40b0fc4ab565d957de34
Denise Case <denisecase@users.noreply.github.com>
2025-01-08 09:05:59 -0600

    Update utils_case.py

--- utils_case.py
+++ utils_case.py
@@ -1,22 +1,53 @@
-'''  FINAL EXAMPLE
+'''  
 Module: Stellar Analytics 
+Author: Denise Case
 This module provides a simple, reusable foundation for my analytics projects. 
+
+TODO: Change the company name in this opening docstring
+TODO: Change the author in this opening docstring
 '''
 
+#####################################
+# Import Modules
+#####################################
+
+# Python Standard library imports
 import statistics
 
+#####################################
+# Declare Global Variables
+#####################################
+
+# declare a boolean variable (has a value True or False)
+# TODO: Add or replace this with your own boolean variable
 has_international_clients: bool = True
+
+# declare an integer variable 
+# TODO: Add or replace this with your own integer variable
 years_in_operation: int = 10
+
+# declare a floating point variable
+# TODO: Add or replace this with your own floating point variable
 average_client_satisfaction: float = 4.7
+
+# declare a list of strings
+# TODO: Add or replace this with your own list  
 skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
+
+# declare a list of numbers so we can illustrate statistics skills
+# TODO: Add or replace this with your own numeric list  
 client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
 
-# Calculate basic statistics using built-in functions and the statistics module
+# Calculate basic statistics using built-in Python functions and the statistics module
+# TODO: Replace these variable names with the variable name of your own numeric list
 min_score: float = min(client_satisfaction_scores)  
 max_score: float = max(client_satisfaction_scores)  
 mean_score: float = statistics.mean(client_satisfaction_scores)  
 stdev_score: float = statistics.stdev(client_satisfaction_scores)
 
+# Use a Python formatted string (f-string) to show information
+# TODO: Modify the text in the byline to fit your information
+# TODO: Modify the variables in the byline to use your variable names
 byline: str = f"""
 ---------------------------------------------------------
 Stellar Analytics: Delivering Professional Insights
@@ -31,13 +62,29 @@ Mean Satisfaction Score: {mean_score:.2f}
 Standard Deviation of Satisfaction Scores: {stdev_score:.2f}
 """
 
+#####################################
+# Define global functions (resuable instructions)
+#####################################
+
 def get_byline() -> str:
     '''Return a byline for my analytics projects.'''
     return byline
 
+#####################################
+# Define main function for this module.
+#####################################
+
 def main() -> None:
     '''Print results of get_byline() when main() is called.'''
+    print("Starting........")
     print(get_byline())
+    print("Complete.......")
+
+#####################################
+# Conditional Execution
+#####################################
 
 if __name__ == '__main__':
     main()
+
+#TODO: Run this as a script to test that all functions work as intended.
