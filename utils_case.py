f633c62943484e8a03c52cd1e017c20137cf2570
Denise Case <denisecase@users.noreply.github.com>
2025-05-12 09:32:53 -0500

    Update utils_case.py

--- utils_case.py
+++ utils_case.py
@@ -32,31 +32,31 @@ import statistics
 # TODO: Add another or replace this with your own boolean variable
 has_international_clients: bool = True
 
-# declare an integer variable 
-# TODO: Add or replace this with your own integer variable
+# declare integer variables
+# TODO: Add another integer variable before this one
 years_in_operation: int = 10
 
-# declare a floating point variable
-# TODO: Add or replace this with your own floating point variable
+# declare a floating point variables
+# TODO: Add another of floating point variable before this one
 average_client_satisfaction: float = 4.7
 
-# declare a list of strings
-# TODO: Add or replace this with your own list  
+# declare lists of strings
+# TODO: Add another list of strings before this one
 skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
 
-# declare a list of numbers so we can illustrate statistics skills
-# TODO: Add or replace this with your own numeric list  
+# declare lists of numbers to illustrate statistics skills
+# TODO: Add another numeric list before this one
 client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
 
 # Calculate basic statistics using built-in Python functions and the statistics module
-# TODO: Replace these variable names with the variable name of your own numeric list
+# TODO: REPLACE the variable name, e.g. client_satisfaction_scores with your own numeric list variable name in all 4 lines below:
 min_score: float = min(client_satisfaction_scores)  
 max_score: float = max(client_satisfaction_scores)  
 mean_score: float = statistics.mean(client_satisfaction_scores)  
 stdev_score: float = statistics.stdev(client_satisfaction_scores)
 
 # Use a Python formatted string (f-string) to show information
-# TODO: Modify the text in the byline to fit your information
+# TODO: Modify the text in the byline to describe your information
 # TODO: Modify the variables in the byline to use your variable names
 byline: str = f"""
 ---------------------------------------------------------
